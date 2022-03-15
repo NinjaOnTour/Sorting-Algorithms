933d6f3b88a705bb908877d73379e9cb1be3bae0
NinjaOnTour <70662656+NinjaOnTour@users.noreply.github.com>
2022-03-15 18:13:55 +0300

    Update main.c
    added Insertion sort and bubble sort

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<time.h>
 #include<stdlib.h>
 
 #define LENGHT 5
@@ -25,9 +24,42 @@ void SelectionSort(int* array)
 	}
 }
 
-void InsertionSort(int* array) 
+void InsertionSort(int* array)
 {
-	
+	int i, j;
+	int temp;
+	for(i = 1; i < LENGHT; i++) // 5 0 10 1 40
+	{
+		for(j = i - 1; j >= 0; j--) 
+		{
+			if(array[i] < array[j]) 
+			{
+				temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+				i--;
+			}
+		}
+	}
+}
+
+void BubbleSort(int* array) 
+{
+	int i, j, n;
+	int temp;
+	for(i = 0; i < LENGHT; i++) 
+	{
+		for(j = 0; j < LENGHT-1; j++) 
+		{
+			n = j+1;
+			if(array[j] > array[n]) 
+			{
+				temp = array[j];
+				array[j] = array[n];
+				array[n] = temp;
+			}
+		}
+	}
 }
 
 void PrintIntArray(int* array, char message[]) 
@@ -57,7 +89,7 @@ int main() {
 			scanf("%d", &numbers[i]);
 		}
 		PrintIntArray(numbers, "");
-		SelectionSort(numbers);
+		SelectionSort(numbers); 			// Sort
 		printf("\nSorted: ");
 		PrintIntArray(numbers, "\n\n");
 	}
